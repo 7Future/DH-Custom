21a356792caf5a92e870dfd48325429555b63e48
Fear <asmarquraishi@gmail.com>
2023-02-18 21:20:51 +0500

    Released Da hood Customs

--- Da Hood Customs.lua
+++ Da Hood Customs.lua
@@ -1,4 +1,53 @@
--- main aimlock settings
+-- getgenv().SecureMode = true
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "DH Customs",
+    LoadingTitle = "Powered by Rayfield Interface Suite // MidNight Exploits",
+    LoadingSubtitle = "by // Fear#8699",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "DH Customs"
+    },
+    Discord = {
+       Enabled = true,
+       Invite = "SVztbETbTs", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+    KeySystem = true, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "DH Customs Key System",
+       Subtitle = "Powered by Rayfield Interface Suite // MidNight Exploits",
+       Note = "Join the discord (discord.gg/GCgefmjzs6)",
+       FileName = "DH Customs Key System",
+       SaveKey = true,
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = "@ADSFDS!!_!!2131312!!_!!AFDFDF@"
+    }
+ })
+ Rayfield:Notify({
+    Title = "DH Customs [BETA]",
+    Content = "JOIN THE DISCORD FOR UPDATE ON THE GUI (discord.gg/GCgefmjzs6)",
+    Duration = 6.5,
+    Image = 4483362458,
+    Actions = { -- Notification Buttons
+       Ignore = {
+          Name = "Okay!",
+          Callback = function()
+          print("Mid Night Exploits")
+       end
+    },
+ },
+ })
+
+ local Tab = Window:CreateTab("Combat", 4483362458) -- Title, Image
+ local Section = Tab:CreateSection("Combat")
+
+ local Button = Tab:CreateButton({
+    Name = "AimLock ",
+    Callback = function()
+    -- The function that takes place when the button is pressed
 local configs = {
     main = {
         enabled = true,
@@ -8,8 +57,6 @@ local configs = {
         notifications = true
     }
 }
-
--- box / marker settings
 
 local boxsettings = {
     box = {
@@ -147,3 +194,75 @@ mt.__namecall = newcclosure(function(...)
     end
     return old(...)
 end)
+    end,
+ })
+
+ local Button = Tab:CreateButton({
+    Name = "AntiKick",
+    Callback = function()
+    -- The function that takes place when the button is pressed
+    --// Cache
+
+local getgenv, getnamecallmethod, hookmetamethod, newcclosure, checkcaller, stringlower = getgenv, getnamecallmethod, hookmetamethod, newcclosure, checkcaller, string.lower
+
+--// Loaded check
+
+if getgenv().ED_AntiKick then return end
+
+--// Variables
+
+local Players, StarterGui, OldNamecall = game:GetService("Players"), game:GetService("StarterGui")
+
+--// Global Variables
+
+getgenv().ED_AntiKick = {
+	SendNotifications = true, -- Set to true if you want to get notified for every event
+	CheckCaller = false -- Set to true if you want to disable kicking by other executed scripts
+}
+
+--// Main
+
+OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(...)
+	if (getgenv().ED_AntiKick.CheckCaller and not checkcaller() or true) and stringlower(getnamecallmethod()) == "kick" then
+		if getgenv().ED_AntiKick.SendNotifications then
+			StarterGui:SetCore("SendNotification", {
+				Title = "DH Customs [BETA],
+				Text = "The script has successfully intercepted an attempted kick.",
+				Icon = "rbxassetid://6238540373",
+				Duration = 2,
+			})
+		end
+
+		return nil
+	end
+
+	return OldNamecall(...)
+end))
+
+if getgenv().ED_AntiKick.SendNotifications then
+	StarterGui:SetCore("SendNotification", {
+		Title = "DH Customs [BETA]",
+		Text = "Anti-Kick script loaded!",
+		Icon = "rbxassetid://6238537240",
+		Duration = 3,
+	})
+end
+    end,
+ })
+
+ local Toggle = Tab:CreateToggle({
+    Name = "Rejoin Game",
+    CurrentValue = false,
+    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+    -- The function that takes place when the toggle is pressed
+    -- The variable (Value) is a boolean on whether the toggle is true or false
+    game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+print("Mid Night Exploits")
+    end,
+ })
+
+
+
+
+Rayfield:LoadConfiguration()
