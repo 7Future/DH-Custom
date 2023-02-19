b911ece0e92c4310bedf94ae769d728789d6a266
Fear <asmarquraishi@gmail.com>
2023-02-19 18:29:57 +0500

    Update AimLock.lua
    Fixed Issues

--- AimLock.lua
+++ AimLock.lua
@@ -46,7 +46,7 @@ Mouse.KeyDown:Connect(function(KeyPressed)
             if configs.main.notifications == true then
                 Plr = FindClosestUser()
                 game.StarterGui:SetCore("SendNotification", {
-                    Title = "DH Customs (Press C To Lock)",
+                    Title = "DH Customs (Press C)",
                     Text = "Unlocked;"
                 })
             end
@@ -55,7 +55,7 @@ Mouse.KeyDown:Connect(function(KeyPressed)
             configs.main.enabled = true
             if configs.main.notifications == true then
                 game.StarterGui:SetCore("SendNotification", {
-                    Title = "DH Customs (Press C To UnLock)",
+                    Title = "DH Customs (Press C)",
                     Text = "Locked On;  " .. tostring(Plr.Character.Humanoid.DisplayName)
                 })
             end
