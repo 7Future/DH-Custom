3171e41e3291e7b98791c8cee8c3fbb89545d6b1
Fear <asmarquraishi@gmail.com>
2023-03-03 21:03:16 +0500

    FIXED ISSUES

--- StreamAble.lua
+++ StreamAble.lua
@@ -1,1 +1,56 @@
-function getplrsname() for i,v in pairs(game:GetChildren()) do if v.ClassName == "Players" then return v.Name end end end local players = getplrsname() local plr = game[players].LocalPlayer coroutine.resume(coroutine.create(function() while wait(1) do coroutine.resume(coroutine.create(function() for _,v in pairs(game[players]:GetPlayers()) do if v.Name ~= plr.Name and v.Character then v.Character.RightUpperLeg.CanCollide = false v.Character.RightUpperLeg.Transparency = 75 v.Character.RightUpperLeg.Size = Vector3.new(21,21,21) v.Character.LeftUpperLeg.CanCollide = false v.Character.LeftUpperLeg.Transparency = 75 v.Character.LeftUpperLeg.Size = Vector3.new(21,21,21) v.Character.HeadHB.CanCollide = false v.Character.HeadHB.Transparency = 75 v.Character.HeadHB.Size = Vector3.new(21,21,21) v.Character.HumanoidRootPart.CanCollide = false v.Character.HumanoidRootPart.Transparency = 75 v.Character.HumanoidRootPart.Size = Vector3.new(21,21,21) end end end)) end end))
+_G.PART = "HumanoidRootPart"
+_G.PRED = 0.13960 
+_G.T = 0.9
+
+local lp = game:GetService("Players").LocalPlayer
+local mouse = lp:GetMouse()
+local enabled = false
+local Target
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+setreadonly(mt, false)
+mt.__namecall =
+newcclosure(
+function(...)
+local args = {...}
+if enabled and getnamecallmethod() == "FireServer" and args[2] == "UpdateMousePos" then
+args[3] = Target[_G.PART].Position + (Target[_G.PART].Velocity * _G.PRED)
+return old(unpack(args))
+end
+return old(...)
+end)
+
+
+game:GetService("RunService").RenderStepped:Connect(function() 
+    
+for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+if v ~= game:GetService("Players").LocalPlayer and v.Character.BodyEffects:FindFirstChild("K.O").Value == false then 
+if mouse.Target == v.Character.HumanoidRootPart then
+    Target = v.Character
+    enabled = true
+end
+end
+if v ~= game:GetService("Players").LocalPlayer then
+if v.Character.BodyEffects:FindFirstChild("K.O").Value == false then 
+v.Character.HumanoidRootPart.Size = Vector3.new(10, 10, 10)
+v.Character.HumanoidRootPart.Transparency = _G.T
+v.Character.HumanoidRootPart.CanCollide = false
+else
+v.Character.HumanoidRootPart.Size = Vector3.new(0.1, 0.1, 0.1)
+v.Character.HumanoidRootPart.Transparency = 1
+v.Character.HumanoidRootPart.CanCollide = false
+end
+if v.Character:FindFirstChild("GRABBING_CONSTRAINT") then
+    v.Character.HumanoidRootPart.Size = Vector3.new(0.1, 0.1, 0.1)
+    v.Character.HumanoidRootPart.Transparency = 1
+    v.Character.HumanoidRootPart.CanCollide = false
+end
+end
+end
+if mouse.Target.Name == "HumanoidRootPart" then
+enabled = true
+else
+enabled = false
+end
+
+end)
